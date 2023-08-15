8c38303830d726b31a3518ecf26c350ee7633db8
FNZhao <122755570+FNZhao@users.noreply.github.com>
2023-08-15 13:24:55 -0400

    Update Navigation.tsx

--- samples/app/react-contoso/src/services/Navigation.tsx
+++ samples/app/react-contoso/src/services/Navigation.tsx
@@ -5,7 +5,9 @@ import {
   TextBulletListSquareRegular,
   CalendarMailRegular,
   DocumentRegular,
-  TagMultipleRegular
+  TagMultipleRegular,
+  CalendarArrowCounterclockwise24Regular,
+  DocumentBulletListMultiple24Regular
 } from '@fluentui/react-icons';
 import { DashboardPage } from '../pages/DashboardPage';
 import { OutlookPage } from '../pages/OutlookPage';
@@ -13,6 +15,9 @@ import { SearchPage } from '../pages/SearchPage';
 import { HomePage } from '../pages/HomePage';
 import { FilesPage } from '../pages/FilesPage';
 import { TaxonomyPage } from '../pages/TaxonomyPage';
+import { CalendarByWeek } from '../pages/CalendarByWeek';
+import CalendarPage from '../pages/CalendarPage';
+import { ChannelFilesPage } from '../pages/ChannelFilesPage';
 
 export const getNavigation = (isSignedIn: boolean) => {
   let navItems: NavigationItem[] = [];
@@ -78,6 +83,37 @@ export const getNavigation = (isSignedIn: boolean) => {
       component: <SearchPage />,
       exact: false
     });
+
+    navItems.push({
+      name: 'Calendar',
+      url: '/calendar',
+      icon: <CalendarArrowCounterclockwise24Regular />,
+      key: 'calendar',
+      requiresLogin: true,
+      component: <CalendarPage />,
+      exact: true
+    });
+
+    navItems.push({
+      name: 'CalendarByWeek',
+      url: '/calendarByWeek',
+      icon: <CalendarArrowCounterclockwise24Regular />,
+      key: 'calendarByWeek',
+      requiresLogin: true,
+      component: <CalendarByWeek />,
+      exact: true
+    });
+
+    navItems.push({
+      name: 'ChannelFiles',
+      url: '/channelFiles',
+      icon: <DocumentBulletListMultiple24Regular />,
+      key: 'channelFiles',
+      requiresLogin: true,
+      component: <ChannelFilesPage />,
+      exact: true
+    });
   }
   return navItems;
 };
+
